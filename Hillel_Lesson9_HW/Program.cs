d7e6dcf57986f72e109b08138cc25b0d090c9431
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-04-26 13:31:53 +0300

    1

--- Hillel_Lesson9_HW/Program.cs
+++ Hillel_Lesson9_HW/Program.cs
@@ -6,12 +6,78 @@ namespace Hillel_Lesson9_HW
     {
         static void Main(string[] args)
         {
+            
+            
+            Random rnd = new Random();
+
+            object lockObject = new object();
+            
+            
+
+            int[] numbers = new int[100000];
+
+            int sumArrayNumbers = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(0, 21);
+                Console.WriteLine("{0}", numbers[i]);
+            }
 
 
 
+            ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+            
+            
 
+            Thread t1 = new Thread(() =>
+            {
+                for (int i = 0; i < 25000; i++)
+                {
+                    lock (lockObject)
+                    {
+                        sumArrayNumbers += numbers[i];
+                    }
+                }
+            });
+            
+            Thread t2 = new Thread(() =>
+            {
+                for (int i = 0; i < 25000; i++)
+                {
+                    lock (lockObject)
+                    {
+                        sumArrayNumbers += numbers[i];
+                    }
+                }
+            });
+            
+            Thread t3 = new Thread(() =>
+            {
+                for (int i = 0; i < 25000; i++)
+                {
+                    lock (lockObject)
+                    {
+                        sumArrayNumbers += numbers[i];
+                    }
+                }
+            });
 
-
+            Thread t4 = new Thread(() =>
+            {
+                for (int i = 0; i < 25000; i++)
+                {
+                    lock (lockObject)
+                    {
+                        sumArrayNumbers += numbers[i];
+                    }
+                }
+            });
+            
+            Thread checkThread = new Thread(() =>
+            {
+                
+            });
 
 
 
