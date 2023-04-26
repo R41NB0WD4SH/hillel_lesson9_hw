1c490d76806289245cd8c57a7d2ba3202d3fe416
R41NB0WD4SH <116415348+R41NB0WD4SH@users.noreply.github.com>
2023-04-26 15:28:32 +0300

    1

--- Hillel_Lesson9_HW/Program.cs
+++ Hillel_Lesson9_HW/Program.cs
@@ -9,6 +9,8 @@
             Random rnd = new Random();
 
             object lockObject = new object();
+
+            bool condition = true;
             
             
 
@@ -19,8 +21,11 @@
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = rnd.Next(0, 21);
-                Console.WriteLine("{0}", numbers[i]);
+                // Console.WriteLine("{0}", numbers[i]);
+
             }
+            
+
 
 
 
@@ -30,53 +35,114 @@
 
             Thread t1 = new Thread(() =>
             {
-                for (int i = 0; i < 25000; i++)
+                manualResetEvent.WaitOne();
+                
+                Console.WriteLine("Thread 1 Started...");
+                
+                if (condition)
                 {
-                    lock (lockObject)
+                    for (int i = 0; i < 25000; i++)
                     {
-                        sumArrayNumbers += numbers[i];
+                        lock (lockObject)
+                        {
+                            sumArrayNumbers += numbers[i];
+                        }
                     }
+                    Console.WriteLine("Thread 1 Finished...");
                 }
             });
             
             Thread t2 = new Thread(() =>
             {
-                for (int i = 0; i < 25000; i++)
+                manualResetEvent.WaitOne();
+                
+                Console.WriteLine("Thread 2 Started...");
+                
+                
+                if (condition)
                 {
-                    lock (lockObject)
+                    for (int i = 25001; i < 50000; i++)
                     {
-                        sumArrayNumbers += numbers[i];
+                        lock (lockObject)
+                        {
+                            sumArrayNumbers += numbers[i];
+                        }
                     }
+                    Console.WriteLine("Thread 2 Finished...");
                 }
             });
             
             Thread t3 = new Thread(() =>
             {
-                for (int i = 0; i < 25000; i++)
+                manualResetEvent.WaitOne();
+                
+                Console.WriteLine("Thread 3 Started...");
+                
+                if (condition)
                 {
-                    lock (lockObject)
+                    for (int i = 50001; i < 75000; i++)
                     {
-                        sumArrayNumbers += numbers[i];
+                        lock (lockObject)
+                        {
+                            sumArrayNumbers += numbers[i];
+                        }
                     }
+                    Console.WriteLine("Thread 3 Finished...");
                 }
             });
 
             Thread t4 = new Thread(() =>
             {
-                for (int i = 0; i < 25000; i++)
+                manualResetEvent.WaitOne();
+                
+                Console.WriteLine("Thread 4 Started...");
+                
+                if (condition)
                 {
-                    lock (lockObject)
+                    for (int i = 75001; i < 100000; i++)
                     {
-                        sumArrayNumbers += numbers[i];
+                        lock (lockObject)
+                        {
+                            sumArrayNumbers += numbers[i];
+                        }
                     }
+                    Console.WriteLine("Thread 4 Calculated...");
                 }
+
             });
             
-            Thread checkThread = new Thread(() =>
+            Thread checkingThread = new Thread(() =>
             {
                 
-            });
+                Console.WriteLine("Cheking Thread Started...");
+                
+                
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] < 1)
+                    {
+                        condition = false;
+                    }
+                }
 
+                manualResetEvent.Set();
+
+            });
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            checkingThread.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+            checkingThread.Join();
+
+
+            Console.WriteLine("Sum: {0}", sumArrayNumbers);
 
 
             Console.ReadKey();
